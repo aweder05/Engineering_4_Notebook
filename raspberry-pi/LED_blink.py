6278e62d4325c4318c7be6cb727a5b0b8196ada6
AntonWeder <aweder05@charlottesvilleschools.org>
2023-09-06 10:28:34 -0400

    finalized led blink code

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -1,13 +1,13 @@
 # type: ignore
 import board
 import digitalio
-led = digitalio.DigitalInOut(board.GP0)
+led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT 
 import time 
 led.
  
 while True: 
-led.value = True 
-time.sleep(200)
-led.value = False
-time.sleep(200)
+    led.value = True 
+    time.sleep(0.200)
+    led.value = False
+    time.sleep(0.200)
