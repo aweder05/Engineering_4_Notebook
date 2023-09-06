32eb76eec2d3868e477785748895c4810fef096e
AntonWeder <aweder05@charlottesvilleschools.org>
2023-09-06 10:40:12 -0400

    wrote some launch pad code before first test

--- raspberry-pi/Lauch_Pad_Part_1.py
+++ raspberry-pi/Lauch_Pad_Part_1.py
@@ -1,4 +1,11 @@
 import board
-
 import time 
-led.
+Seconds = 10
+
+while True: 
+    if Seconds > 0 
+        print("T-Minus", Seconds)
+        time.sleep(1)
+        Seconds = Seconds - 1 
+    if else:
+        print("WE HAVE LIFTOFF")
