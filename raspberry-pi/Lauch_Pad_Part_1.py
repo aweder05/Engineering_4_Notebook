e77e02fe44f153ed4400f21a00ee77b0efe38ff1
AntonWeder <aweder05@charlottesvilleschools.org>
2023-09-07 10:25:01 -0400

    commented code

--- raspberry-pi/Lauch_Pad_Part_1.py
+++ raspberry-pi/Lauch_Pad_Part_1.py
@@ -1,11 +1,13 @@
+# type: ignore
 import board
 import time 
-Seconds = 10
+Seconds = 10 ## Sets the amount of time that the code is counting down from 
 
 while True: 
-    if Seconds > 0 
-        print("T-Minus", Seconds)
-        time.sleep(1)
+    if Seconds > 0: 
+        print("T-Minus", Seconds) ## Prints the amount of seconds remaining
+        time.sleep(1)  ## Subracts 1 from "Seconds" each time one second passes
         Seconds = Seconds - 1 
-    if else:
-        print("WE HAVE LIFTOFF")
+    else:
+        print("WE HAVE LIFTOFF") ## Prints liftoff statement once the ticker reaches zero 
+        time.sleep(1)
