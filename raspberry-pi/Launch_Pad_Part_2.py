1fdf5bf88ff7b4ac3ab42963f9adf17622d1cb6a
AntonWeder <aweder05@charlottesvilleschools.org>
2023-09-07 10:47:20 -0400

    code launch pad pt 2

---
+++ raspberry-pi/Launch_Pad_Part_2.py
@@ -0,0 +1,22 @@
+## Note: The following code up until "time.sleep(1)" is from Launch Pad Part 1, the previous assignment 
+# type: ignore
+import board
+import time 
+import digitalio
+
+ledGreen = digitalio.DigitalInOut(board.GPO)
+ledRed = digitalio.DigitalInOut(board.GP1)
+
+ledGreen.direction = digitalio.Direction.OUTPUT 
+ledRed.direction = digitalio.Direction.OUTPUT  
+
+Seconds = 10 ## Sets the amount of time that the code is counting down from 
+
+while True: 
+    if Seconds > 0: 
+        print("T-Minus", Seconds) ## Prints the amount of seconds remaining
+        time.sleep(1)  ## Subracts 1 from "Seconds" each time one second passes
+        Seconds = Seconds - 1 
+    else:
+        print("WE HAVE LIFTOFF") ## Prints liftoff statement once the ticker reaches zero 
+        time.sleep(1)
