d68fbe65e416268cee7ea3c128f1f73508e78881
AntonWeder <aweder05@charlottesvilleschools.org>
2023-09-11 10:12:19 -0400

    commented code

--- raspberry-pi/Launch_Pad_Part_2.py
+++ raspberry-pi/Launch_Pad_Part_2.py
@@ -4,8 +4,8 @@ import board
 import time 
 import digitalio
 
-ledGreen = digitalio.DigitalInOut(board.GP0)
-ledRed = digitalio.DigitalInOut(board.GP1)
+ledGreen = digitalio.DigitalInOut(board.GP0) ## Sets the pin number for the Green LED
+ledRed = digitalio.DigitalInOut(board.GP1) ## Sets the pin number for the Red LED
 
 ledGreen.direction = digitalio.Direction.OUTPUT 
 ledRed.direction = digitalio.Direction.OUTPUT  
@@ -15,13 +15,13 @@ Seconds = 10 ## Sets the amount of time that the code is counting down from
 while True: 
     if Seconds > 0: 
         print("T-Minus", Seconds) ## Prints the amount of seconds remaining
-        ledRed.value = True 
+        ledRed.value = True ## Turns on the Red LED 
         time.sleep(.5)
-        ledRed.value = False 
-        time.sleep(.5)
-        Seconds = Seconds - 1 
+        ledRed.value = False ## Turns off the Red LED for the flashing effect 
+        time.sleep(.5) 
+        Seconds = Seconds - 1 ## Subracts one second of "Seconds"
     else:
-        ledGreen.value = True 
-        ledRed.value = False
+        ledGreen.value = True ## Turns on the Green LED since the timer has reached zero
+        ledRed.value = False ## Stops the flashing of the Red LED
         print("WE HAVE LIFTOFF") ## Prints liftoff statement once the ticker reaches zero 
-        time.sleep(1)
+        time.sleep(1) 
