c39f92d4caf08e18f9d5bc997a1b61b090dc56a2
AntonWeder <aweder05@charlottesvilleschools.org>
2023-10-30 10:08:38 -0400

    aasd

--- raspberry-pi/LandingAreaPart1(Functions).py
+++ raspberry-pi/LandingAreaPart1(Functions).py
@@ -1,5 +1,40 @@
-# type: ignore
+#type: ignore
+
 import board
 
-while True: 
-    print("hello")
+def TriArea(x1,y1,x2,y2,x3,y3): #Creates a function to find the area
+    try: #Find the area if coordinates are valid
+        area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) #Triangle area formula
+        return area
+    except: #If coordinates are invalid
+        print("Invalid Coordinates")
+        area = 0
+        return area
+
+while True:
+    print("Point 1 Coordinates (x,y):") #Print so the perwson running code knows which coordinates
+    p1 = input() #Return whatever the user types in
+    x1, y1 = p1.split(",") #Turn p1 into two variables separated by a comma
+    print(f"Point 1: ({p1})") #Confirm the coordinates
+
+    print("Point 2 Coordinates (x,y):") #Repeat for coordinate 2
+    p2 = input()
+    x2, y2 = p2.split(",")
+    print(f"Point 2: ({p2})")
+
+    print("Point 3 Coordinates (x,y):") #Repeat for coordinate 3
+    p3 = input()
+    x3, y3 = p3.split(",")
+    print(f"Point 3: ({p3})")
+
+    x1 = float(x1) #Turn varibles from strings into floats so they can be plugged into the function
+    x2 = float(x2)
+    x3 = float(x3)
+    y1 = float(y1)
+    y2 = float(y2)
+    y3 = float(y3)
+    
+    area = TriArea(x1,y1,x2,y2,x3,y3) #Plug in floats to find area
+    print(" ")
+    print(f"AREA IS: {area}")
+    print(" ")
